a576275c29f59a3e6bf0bb18719cd295273aef52
Redfire <redfire75369@hotmail.com>
2022-12-06 14:48:10 +0800

    2022 Day 6

---
+++ 2022/day06/index.ts
@@ -0,0 +1,27 @@
+import {sample, full} from "./inputs";
+
+const inputs = full;
+
+function uniqueWindowIndex(length: number): number {
+	for (let i = 0; i < inputs.length; i++) {
+		const window = inputs.slice(i, i + length);
+		if (window.every(w => window.filter(w2 => w === w2).length === 1)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+/* Part 1 */
+function part1() {
+	return uniqueWindowIndex(4) + 4;
+}
+
+console.log("Part 1: " + part1());
+
+/* Part 2 */
+function part2() {
+	return uniqueWindowIndex(14) + 14;
+}
+
+console.log("Part 2: " + part2());
