3766e2e30126439ef068636c683cd1294211fe68
Redfire <redfire75369@hotmail.com>
2022-12-16 01:00:32 +0800

    Added 2022 Day 15

---
+++ 2022/day15/index.ts
@@ -0,0 +1,71 @@
+import {sample, full} from "./inputs";
+import {sum} from "../../utils/reducer";
+
+const inputs = full;
+const isFull = inputs.length === full.length;
+
+function pointsOnRow(row: number, sensor: [number, number], radius: number): [number, number] | number {
+	const dy = Math.abs(row - sensor[1]);
+	if (radius < dy) {
+		return null;
+	}
+	if (radius === dy) {
+		return [sensor[0], sensor[0]];
+	}
+
+	const leftX = sensor[0] - (radius - dy);
+	const rightX = sensor[0] + (radius - dy);
+
+	return [leftX, rightX];
+}
+
+function mergeIntervals(intervals: [number, number][]): [number, number][] {
+	intervals.sort((a, b) => a[0] - b[0]);
+	const merged = [intervals[0]];
+
+	for (let i = 1; i < intervals.length; i++) {
+		 const previous = merged[merged.length - 1];
+		 if (previous[1] >= intervals[i][0]) {
+			 previous[1] = Math.max(previous[1], intervals[i][1]);
+		 } else {
+			 merged.push(intervals[i]);
+		 }
+	}
+	return merged;
+}
+
+function intervalsOnRow(row: number): [number, number][] {
+	const intervals = [];
+	inputs.forEach(input => {
+		const {sensor, beacon} = input;
+		const radius = Math.abs(sensor[0] - beacon[0]) + Math.abs(sensor[1] - beacon[1]);
+		const points = pointsOnRow(row, sensor, radius);
+		if (points !== null) {
+			intervals.push(points);
+		}
+	});
+	return mergeIntervals(intervals);
+}
+
+/* Part 1 */
+function part1() {
+	const intervals = intervalsOnRow(isFull ? 2000000 : 10);
+	return intervals.reduce(sum(i => i[1] - i[0]), 0);
+}
+
+console.log("Part 1:", part1());
+
+/* Part 2 */
+function part2() {
+	let point = null;
+	for (let y = 0; y < (isFull ? 4000000 : 20); y++) {
+		const intervals = intervalsOnRow(y);
+		if (intervals.length > 1) {
+			point = [intervals[0][1] + 1, y];
+			break;
+		}
+	}
+	return point[0] * 4000000 + point[1];
+}
+
+console.log("Part 2:", part2());
---
+++ 2022/day15/inputs.ts
@@ -0,0 +1,36 @@
+import {readFileSync} from "fs";
+import {join} from "path";
+
+interface Input {
+	sensor: [number, number],
+	beacon: [number, number]
+}
+
+type Inputs = Input[];
+
+function parseInputs(filePath: string): Inputs {
+	return readFileSync(join(__dirname, filePath), {encoding: "utf8"}).trim().split("\n")
+		.map(line => {
+			const commaIndex = line.indexOf(",");
+			const colonIndex = line.indexOf(":");
+
+			const sensorX = parseInt(line.substring(line.indexOf("x=") + 2, commaIndex));
+			const sensorY = parseInt(line.substring(line.indexOf("y=") + 2, colonIndex));
+
+			const beaconX = parseInt(line.substring(line.indexOf("x=", commaIndex) + 2, line.indexOf(",", commaIndex + 1)));
+			const beaconY = parseInt(line.substring(line.indexOf("y=", colonIndex) + 2, line.length));
+
+			return {
+				sensor: [sensorX, sensorY],
+				beacon: [beaconX, beaconY],
+			}
+		});
+}
+
+const sample = parseInputs("./sample.txt");
+const full = parseInputs("./full.txt");
+
+export {
+	sample,
+	full,
+};
